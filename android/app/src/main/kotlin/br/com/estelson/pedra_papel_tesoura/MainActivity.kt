ba7eab4702e663b4f4eebfef59c23a97cc5205c9
Estelson Medeiros Pereira <estelson@gmail.com>
2023-03-23 09:20:56 -0300

    Initial commit
    Aplicação baseada no curso Udemy - Desenvolvimento Android e IOS com Flutter - Crie 15 Apps - https://www.udemy.com/course/desenvolvimento-android-e-ios-com-flutter - Seção 10, Aula 67

---
+++ android/app/src/main/kotlin/br/com/estelson/pedra_papel_tesoura/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.estelson.pedra_papel_tesoura
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Pedra, Papel, Tesoura',
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+      ),
+      home: const MyHomePage(title: 'Pedra, Papel, Tesoura'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: const SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            physics: BouncingScrollPhysics(),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Pedra, Papel, Tesoura',
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
